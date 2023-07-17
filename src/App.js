7c3114964bbadab08d138ab2842eeae8b322fd40
rupeshgondrawar <rupeshgondrawar462@gmail.com>
2023-07-17 12:35:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const getData = (page) => {
+  return fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`).then(
+    (res) => {
+      return res.json();
+    }
+  );
+};
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [page,setPage]=useState(1);
+
+  useEffect(() => {
+    fetchAndUpdateData(page);
+  }, [page]);
+
+  const fetchAndUpdateData = async (page=1) => {
+    try {
+      setLoading(true);
+      const data = await getData(page);
+      setPosts(data);
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  };
+
+  const handleChange=(changeBy)=>{
+      setPage(page+changeBy);
+  }
+
+  if(loading){
+    return <img src="https://icon-library.com/images/loading-gif-icon/loading-gif-icon-9.jpg"/>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Posts</h1>
+
+      <ul>
+        {posts.map((postItem) => (
+          <li key={postItem.id}>{postItem.id} {"-"}  {postItem.title}</li>
+        ))}
+      </ul>
+
+      <button disabled={page===1} onClick={()=>handleChange(-1)}>prev</button>
+      <button>{page}</button>
+      <button disabled={page===10} onClick={()=>handleChange(1)}>next</button>
     </div>
   );
 }
